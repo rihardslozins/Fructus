573d2c4f7a116bc2222767db7bf30eae613fed79
rihardslozins <rihards.lozins@gmail.com>
2021-03-24 16:13:39 +0200

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Rihards Lozins on 24/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
