2a18f318ab3c6d0b9e747c750137fa4420958735
rihardslozins <rihards.lozins@gmail.com>
2021-04-01 17:41:34 +0300

    New Views added + Finished App

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     
     // MARK: - PROPERTIES
     
+    @State private var isShowingSettings: Bool = false
+    
     var fruits: [Fruit] = fruitsData
     
     // MARK: - BODY
@@ -27,7 +29,19 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Fruits")
-        }
+            .navigationBarItems(trailing: Button(action: {
+                isShowingSettings = true
+            }) {
+                Image(systemName: "slider.horizontal.3")
+            } //: BUTTON
+            
+            .sheet(isPresented: $isShowingSettings) {
+                SettingsView()
+            }
+            
+            )
+        } //: NAVIGATION
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
